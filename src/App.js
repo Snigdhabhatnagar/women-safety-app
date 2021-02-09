ea3bbbe313949befa636004fd7c7e89c87b78fe0
Snigdhabhatnagar <snigdha.bhatnagar312@gmail.com>
2021-02-09 16:48:49 +0530

    Sign Up UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+import Register from './Containers/Register/Register';
+import Input from './Components/Input/Input';
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    
+    <Register/>
+      
+   
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+
+export default function Button() {
+  const {
+    error,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    crossBrowser: true,
+    googleApiKey: "AIzaSyBtKHZSd57mVAJa12yKnHaI-45v1Nqnk-s",
+    timeout: 10000,
+  });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
